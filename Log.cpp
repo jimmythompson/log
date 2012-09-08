cb5a627f5b31750739546c042b98d2ee9cdb9878
Jimmy Thompson <jimmy.thompson@hotmail.co.uk>
2012-09-08 14:29:55 +0100

    Doxygen Comments

--- Log.cpp
+++ Log.cpp
@@ -2,6 +2,9 @@
 #include <ctime>
 #include <iostream>
 
+/**
+ * @brief Constructor
+ */
 Log::Log()
 	: m_threshold( INFO ),
 	  m_fileName(),
@@ -9,51 +12,93 @@ Log::Log()
 	  m_stream() {
 }
 
+/**
+ * @brief Copy constructor
+ * @details Kept private in order to preserve singleton
+ */
 Log::Log(const Log&) {
 }
 
+/**
+ * @brief Destructor
+ * @details Logs the shut down then closes the file stream
+ */
 Log::~Log() {
 	if( m_initialised ) {
+		info( "Log shutting down" );
 		m_stream.close();
 	}
 }
 
+/**
+ * @brief Get the singleton instance
+ */
 Log& Log::instance() {
 	static Log log;
 	return log;
 }
 
+/**
+ * @brief Initialises the file stream
+ *
+ * @param fileName The location of the file to create/append to
+ * @return True if the file was successfully initialised; false if already initialised
+ */
 bool Log::initialise( const std::string& fileName ) {
 	if( !m_initialised ) {
 		m_fileName = fileName;
 		m_stream.open( fileName.c_str(), std::ios_base::app | std::ios_base::out );
 		m_initialised = true;
+		info( "Log initialised" );
 		return true;
 	}
 	return false;
 }
 
+/**
+ * @brief Initialises the file stream
+ *
+ * @param fileName The location of the file to create/append to
+ * @return True if the file was successfully initialised; false if already initialised
+ */
 bool Log::initialise( const char* fileName ) {
 	return initialise( std::string( fileName ) );
 }
 
+/**
+ * @brief Writes the specified message to the console and the log file
+ *
+ * @param message The message to write
+ */
 void Log::write( const std::string& message ) {
 	std::cout << message << std::endl;
 	m_stream  << message << std::endl;
 }
 
+/**
+ * @brief Writes the specified message to the console and the log file
+ *
+ * @param message The message to write
+ */
 void Log::write( const char* message ) {
 	write( std::string( message ) );
 }
 
+/**
+ * @brief Logs the specified message with a timestamp and category prefix
+ *
+ * @param type The category of message to write based on the enum Log::Type
+ * @param message The message to log
+ * @return True if the log was successful
+ */
 bool Log::log( const Type type, const std::string& message ) {
 	if( type <= m_threshold ) {
 		std::string prefix( "[ TIME ] " );
-		prefix.append(m_stack.size(), ' ');
+		//prefix.append(m_stack.size(), ' ');
 
 		write( prefix + message );
 
-		//If it's an error, dump the stack.
+		//If it's an error (or worse), dump the stack.
 		if( type <= ERROR ) {
 			write( "====== Stack Trace ======" );
 			for( std::vector<std::string>::reverse_iterator i = m_stack.rbegin(); i != m_stack.rend(); ++i) {
@@ -66,53 +111,151 @@ bool Log::log( const Type type, const std::string& message ) {
 	return false;
 }
 
+/**
+ * @brief Writes a fatal error to the log
+ *
+ * @param message The message to log
+ * @return True if the log was successful
+ */
+bool Log::fatal( const std::string& message ) {
+	return log( FATAL, message );
+}
+
+/**
+ * @brief Writes a fatal error to the log
+ *
+ * @param message The message to log
+ * @return True if the log was successful
+ */
+bool Log::fatal( const char* message ) {
+	return fatal( std::string( message ) );
+}
+
+/**
+ * @brief Writes an error to the log
+ *
+ * @param message The message to log
+ * @return True if the log was successful
+ */
 bool Log::error( const std::string& message ) {
 	return log( ERROR, message );
 }
 
+/**
+ * @brief Writes an error to the log
+ *
+ * @param message The message to log
+ * @return True if the log was successful
+ */
 bool Log::error( const char* message ) {
 	return error( std::string( message ) );
 }
 
+/**
+ * @brief Writes a warning to the log
+ *
+ * @param message The message to log
+ * @return True if the log was successful
+ */
+bool Log::warn( const std::string& message ) {
+	return log( WARN, message );
+}
+
+/**
+ * @brief Writes a warning to the log
+ *
+ * @param message The message to log
+ * @return True if the log was successful
+ */
+bool Log::warn( const char* message ) {
+	return warn( std::string( message ) );
+}
+
+/**
+ * @brief Writes an information message to the log
+ *
+ * @param message The message to log
+ * @return True if the log was successful
+ */
 bool Log::info( const std::string& message ) {
 	return log( INFO, message );
 }
 
+/**
+ * @brief Writes an information message to the log
+ *
+ * @param message The message to log
+ * @return True if the log was successful
+ */
 bool Log::info( const char* message ) {
 	return info( std::string( message ) );
 }
 
+/**
+ * @brief Writes a debug message to the log
+ *
+ * @param message The message to log
+ * @return True if the log was successful
+ */
 bool Log::debug( const std::string& message ) {
 	return log( DEBUG, message );
 }
 
+/**
+ * @brief Writes a debug message to the log
+ *
+ * @param message The message to log
+ * @return True if the log was successful
+ */
 bool Log::debug( const char* message ) {
 	return debug( std::string( message ) );
 }
 
+/**
+ * @brief Peeks at the top element of the function stack
+ *
+ * @return The top element of the function stack
+ */
 std::string Log::peek() {
 	return m_stack.back();
 };
 
+/**
+ * @brief Pushes the function stack with the given message
+ *
+ * @param input The message to store in the stack (typically the name of the function)
+ * @return True if the stack was successfully pushed
+ */
 bool Log::push( const std::string& input ) {
 	if( !input.empty() ) {
-		info( "[BEGIN] " + input );
+		info( "BEGIN - " + input );
 		m_stack.push_back( input );
 		return true;
 	}
 	return false;
 }
 
+/**
+ * @brief Pushes the function stack with the given message
+ *
+ * @param input The message to store in the stack (typically the name of the function)
+ * @return True if the stack was successfully pushed
+ */
 bool Log::push( const char* input ) {
 	return push( std::string( input ) );
 }
 
-bool Log::pop() {
+/**
+ * @brief Pops the top element off the stack
+ *
+ * @return The message just popped off the stack
+ */
+std::string Log::pop() {
 	if( !m_stack.empty() ) {
 		std::string temp( peek() );
 		m_stack.pop_back();
-		info( "[END] " + temp );
-		return true;
+		info( "END - " + temp );
+		return temp;
 	}
-	return false;
+	return std::string();
 }
--- Log.h
+++ Log.h
@@ -5,9 +5,30 @@
 #include <string>
 #include <vector>
 
+/**
+ * @def PUSH_LOG_STACK
+ * @brief Pushes the logging stack with the function name that called it
+ */
 #define PUSH_LOG_STACK Log::instance().push( __FUNCTION__ );
+
+/**
+ * @def POP_LOG_STACK
+ * @brief Pops the logging stack
+ */
 #define POP_LOG_STACK  Log::instance().pop();
 
+/**
+ * @class Log
+ * @brief Basic logging class
+ * @author Jimmy Thompson
+ *
+ * @version $Revision: 1.0 $
+ * @date $Date: 2012/09/08 - 14:25:00 $
+ * 
+ * @details Logging class to deal with basic string messages and
+ * @details macro-based stack tracing. Something quick I made to
+ * @details handle logging in my own programs; made to be basic.
+ */
 class Log {
 	public:
 		static const enum Type {
@@ -17,6 +38,7 @@ class Log {
 			INFO,
 			DEBUG
 		};
+
 	private:
 		Type m_threshold;
 		bool m_initialised;
@@ -32,25 +54,33 @@ class Log {
 		void write( const char* message );
 
 		bool log( const Type type, const std::string& message );
+
 	public:
 		static Log& instance();
 
 		bool initialise( const std::string& fileName );
 		bool initialise( const char* fileName );
 
+		bool fatal( const std::string& message );
+		bool fatal( const char* message );
+
 		bool error( const std::string& message );
 		bool error( const char* message );
 
+		bool warn( const std::string& message );
+		bool warn( const char* message );
+
 		bool info( const std::string& message );
 		bool info( const char* message );
-
+		
 		bool debug( const std::string& message );
 		bool debug( const char* message );
 
+		//Prefix to string
 		std::string peek();
 		bool push(const std::string& input);
 		bool push(const char* input);
-		bool pop();
+		std::string pop();
 };
 
 #endif
