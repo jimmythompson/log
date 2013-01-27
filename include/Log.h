909e38eb79ab3c9a668fd5020437675cece913b3
JimmyThompson <jimmy.thompson@hotmail.co.uk>
2013-01-27 08:30:37 +0000

    Cleaning, safety etc.
    A lot less changes than the diff is indicating. Knocked everything
    <80px; fiddled with the comments; changed the order of the functions.
    etc.

--- include/Log.h
+++ include/Log.h
@@ -12,12 +12,12 @@
  * @brief Pushes the logging stack with the function name that called it
  */
 #define PUSH_LOG_STACK \
-	StackLogger stackLoggerTempObject = StackLogger( __FUNCTION__ );
+	const StackLogger stackLoggerTempObject = StackLogger( __FUNCTION__ );
 
 /**
  * @class Log
  * @brief Basic logging class
- * @author Jimmy Thompson
+ * @author Jimmy Thompson (http://jimmythompson.co.uk)
  * @author wuqiong
  *
  * @version $Revision: 1.1 $
@@ -25,77 +25,77 @@
  * 
  * @details Logging class to deal with basic string messages and macro-based
  * @details stack tracing. Something quick I made to handle logging in my own
- * @details programs; made to be basic.
+ * @details programs.
  */
 class Log {
-	public:
-		static const enum Type {
-			LOG_TYPE_FATAL,
-			LOG_TYPE_ERROR,
-			LOG_TYPE_WARN,
-			LOG_TYPE_INFO,
-			LOG_TYPE_DEBUG
-		};
+public:
+    static const enum Type {
+        LOG_TYPE_FATAL,
+        LOG_TYPE_ERROR,
+        LOG_TYPE_WARN,
+        LOG_TYPE_INFO,
+        LOG_TYPE_DEBUG
+    };
 
-	private:
-		Type m_threshold;
-		bool m_initialised;
-		std::string m_fileName;
-		std::vector<std::string> m_stack;
-		std::ofstream m_stream;
+    static const char* TypeToString(const Type& type);
 
-		Log();
-		Log(const Log&);
-		~Log();
+    static bool Initialise(const std::string& fileName);
+    static bool Finalise();
 
-		static Log& get();
+    static void SetThreshold(const Type& type);
 
-		void write(const char* format, ...);
+    static bool Fatal(const std::string& message);
+    static bool Fatal(const char* format, ...);
 
-		bool log(const Type& type, const std::string& message);
-		bool log(const Type& type, const char* format, const va_list& varArgs);
+    static bool Error(const std::string& message);
+    static bool Error(const char* format, ...);
 
-		Log& operator=(const Log&);
+    static bool Warn(const std::string& message);
+    static bool Warn(const char* format, ...);
 
-	public:
-		static const char* TypeToString(const Type& type);
+    static bool Info(const std::string& message);
+    static bool Info(const char* format, ...);
 
-		static bool Initialise(const std::string& fileName);
-		static bool Finalise();
+    static bool Debug(const std::string& message);
+    static bool Debug(const char* format, ...);
 
-		static void SetThreshold(const Type& type);
-		static bool Fatal(const std::string& message);
-		static bool Fatal(const char* format, ...);
+    static std::string Peek();
+    static bool Push(const std::string& input);
+    static std::string Pop();
+    static void PrintStackTrace();
 
-		static bool Error(const std::string& message);
-		static bool Error(const char* format, ...);
+private:
+    Type m_threshold;
+    bool m_initialised;
+    std::string m_fileName;
+    std::vector<std::string> m_stack;
+    std::ofstream m_stream;
 
-		static bool Warn(const std::string& message);
-		static bool Warn(const char* format, ...);
+    Log();
+    Log(const Log&);
+    ~Log();
 
-		static bool Info(const std::string& message);
-		static bool Info(const char* format, ...);
+    static Log& get();
 
-		static bool Debug(const std::string& message);
-		static bool Debug(const char* format, ...);
+    void write(const char* format, ...);
 
-		static std::string Peek();
-		static bool Push(const std::string& input);
-		static std::string Pop();
-		static void PrintStackTrace();
+    bool log(const Type& type, const std::string& message);
+    bool log(const Type& type, const char* format, const va_list& varArgs);
+
+    Log& operator=(const Log&);
 };
 
 struct StackLogger
 {
-	StackLogger(const std::string& input)
-	{
-		Log::Push( input );
-	}
+    StackLogger(const std::string& input)
+    {
+        Log::Push( input );
+    }
 
-	~StackLogger()
-	{
-		Log::Pop();
-	}
+    ~StackLogger()
+    {
+        Log::Pop();
+    }
 };
 
 #endif
--- src/Log.cpp
+++ src/Log.cpp
@@ -3,139 +3,10 @@
 #include <iostream>
 
 /**
- * @brief Constructor
- */
-Log::Log()
-	: m_threshold( LOG_TYPE_INFO ),
-	  m_fileName(),
-	  m_stack(),
-	  m_stream() {
-}
-
-/**
- * @brief Copy constructor
- * @details Kept private in order to preserve singleton
- */
-Log::Log(const Log&) {
-}
-
-/**
- * @brief Destructor
- * @details Logs the shut down then closes the file stream
- */
-Log::~Log() {
-	Finalise();
-}
-
-/**
- * @brief Get the singleton instance
- */
-Log& Log::get() {
-	static Log log;
-	return log;
-}
-
-/**
- * @brief Writes the specified message to the console and the log file
+ * @brief   Gets the name of the log category from the enum value
  *
- * @param format The format of the message
- * @param ... Variable arguments
- */
-void Log::write( const char* format, ... ) {
-	char buffer[512];
-
-	va_list varArgs;
-	va_start( varArgs, format );
-	vsnprintf( buffer, sizeof(buffer), format, varArgs);
-	va_end( varArgs );
-
-	std::cout << buffer << std::endl;
-	m_stream  << buffer << std::endl;
-}
-
-
-/**
- * @brief Logs the specified message with a timestamp and category prefix
- * @details The constant TIMESTAMP_BUFFER_SIZE was calculated as the maximum
- * @details number of characters required for the timestamp
- * @details "[HH:MM:SS MM/DD/YY] "
- *
- * @param type The category of message to write based on the enum Log::Type
- * @param message The message to log
- * @return True if the log was successful
- */
-bool Log::log( const Type& type, const std::string& message ) {
-	static const int TIMESTAMP_BUFFER_SIZE = 21;
-	
-	if( type <= m_threshold ) {
-		char buffer[TIMESTAMP_BUFFER_SIZE];
-		time_t timestamp;
-		time( &timestamp );
-		strftime( buffer, sizeof( buffer ), "%X %x", localtime( &timestamp ) );
-
-		write( "[%s] %s - %s", buffer, TypeToString( type ), message.c_str() );
-		return true;
-	}
-	return false;
-}
-
-/**
- * @brief Logs the specified message with a timestamp and category prefix
- * @details The constant TIMESTAMP_BUFFER_SIZE was calculated as the maximum
- * @details number of characters required for the timestamp
- * @details "[HH:MM:SS MM/DD/YY] "
- *
- * @param type The category of message to write based on the enum Log::Type
- * @param format The format of the message
- * @param ... Variable arguments
- * @return True if the log was successful
- */
-bool Log::log( const Type& type, const char* format, const va_list& varArgs) {
-	char buffer[512];
-	vsnprintf( buffer, sizeof(buffer), format, varArgs);
-	return log( type, buffer );
-}
-
-/**
- * @brief Initialises the file stream
- *
- * @param fileName The location of the file to create/append to
- * @return True if the file was successfully initialised; false if already initialised
- */
-bool Log::Initialise( const std::string& fileName ) {
-	Log& log = Log::get();
-
-	if( !log.m_initialised ) {
-		log.m_fileName = fileName;
-		log.m_stream.open( fileName.c_str(), std::ios_base::app | std::ios_base::out );
-		log.m_initialised = true;
-		Info( "LOG INITIALISED" );
-		return true;
-	}
-	return false;
-}
-
-/**
- * @brief Finalises the file stream
- *
- * @return True if the file was successfully finalised; false if not initialised
- */
-bool Log::Finalise() {
-	Log& log = Log::get();
-
-	if( log.m_initialised ) {
-		Info( "LOG FINALISED" );
-		log.m_stream.close();
-		return true;
-	}
-	return false;
-}
-
-/**
- * @brief Gets the name of the log category from the enum value
- *
- * @param The enum value of the category
- * @return The name of the category; returns the word UNKNOWN if not valid.
+ * @param   The enum value of the category
+ * @return  The name of the category; returns the word UNKNOWN if not valid.
  */
 const char* Log::TypeToString( const Type& type ) {
 	switch( type ) {
@@ -156,12 +27,47 @@ const char* Log::TypeToString( const Type& type ) {
 }
 
 /**
- * @brief Sets the debugging threshold
- * @details This is the debugging threshold to use when reporting bugs, useful
- * @details for having lots of debugging information that sometimes you just
- * @details want to turn off.
+ * @brief   Initialises the file stream
  *
- * @param type The given debugging threshold to use
+ * @param   fileName The location of the file to create/append to
+ * @return  True if the file was successfully initialised; false if already initialised
+ */
+bool Log::Initialise( const std::string& fileName ) {
+	Log& log = Log::get();
+
+	if( !log.m_initialised ) {
+		log.m_fileName = fileName;
+		log.m_stream.open( fileName.c_str(), 
+			std::ios_base::app | std::ios_base::out );
+		log.m_initialised = true;
+		Info( "LOG INITIALISED" );
+		return true;
+	}
+	return false;
+}
+
+/**
+ * @brief   Finalises the file stream
+ *
+ * @return  True if the file was successfully finalised; false if not initialised
+ */
+bool Log::Finalise() {
+	Log& log = Log::get();
+
+	if( log.m_initialised ) {
+		Info( "LOG FINALISED" );
+		log.m_stream.close();
+		return true;
+	}
+	return false;
+}
+
+/**
+ * @brief  Sets the debugging threshold
+ * This is the debugging threshold to use when reporting bugs, useful for having
+ * lots of debugging information that sometimes you just want to turn off.
+ *
+ * @param  type The given debugging threshold to use
  */
 void Log::SetThreshold( const Type& type ) {
 	Log& log = Log::get();
@@ -169,21 +75,21 @@ void Log::SetThreshold( const Type& type ) {
 }
 
 /**
- * @brief Writes a Fatal Error to the log
+ * @brief   Writes a Fatal Error to the log
  *
- * @param message The message to log
- * @return True if the log was successful
+ * @param   message The message to log
+ * @return  True if the log was successful
  */
 bool Log::Fatal( const std::string& message ) {
 	return Log::get().log( LOG_TYPE_FATAL, message );
 }
 
 /**
- * @brief Writes a Fatal Error to the log
+ * @brief   Writes a Fatal Error to the log
  *
- * @param format The format of the message
- * @param ... Variable arguments
- * @return True if the log was successful
+ * @param   format The format of the message
+ * @param   ... Variable arguments
+ * @return  True if the log was successful
  */
 bool Log::Fatal( const char* format, ... ) {
 	va_list varArgs;
@@ -194,21 +100,21 @@ bool Log::Fatal( const char* format, ... ) {
 }
 
 /**
- * @brief Writes an Error to the log
+ * @brief   Writes an Error to the log
  *
- * @param message The message to log
- * @return True if the log was successful
+ * @param   message The message to log
+ * @return  True if the log was successful
  */
 bool Log::Error( const std::string& message ) {
 	return Log::get().log( LOG_TYPE_ERROR, message );
 }
 
 /**
- * @brief Writes an Error to the log
+ * @brief  Writes an Error to the log
  *
- * @param format The format of the message
- * @param ... Variable arguments
- * @return True if the log was successful
+ * @param   format The format of the message
+ * @param   ... Variable arguments
+ * @return  True if the log was successful
  */
 bool Log::Error( const char* format, ... ) {
 	va_list varArgs;
@@ -219,21 +125,21 @@ bool Log::Error( const char* format, ... ) {
 }
 
 /**
- * @brief Writes a warning to the log
+ * @brief   Writes a warning to the log
  *
- * @param message The message to log
- * @return True if the log was successful
+ * @param   message The message to log
+ * @return  True if the log was successful
  */
 bool Log::Warn( const std::string& message ) {
 	return Log::get().log( LOG_TYPE_WARN, message );
 }
 
 /**
- * @brief Writes a warning to the log
+ * @brief   Writes a warning to the log
  *
- * @param format The format of the message
- * @param ... Variable arguments
- * @return True if the log was successful
+ * @param   format The format of the message
+ * @param   ... Variable arguments
+ * @return  True if the log was successful
  */
 bool Log::Warn( const char* format, ... ) {
 	va_list varArgs;
@@ -244,21 +150,21 @@ bool Log::Warn( const char* format, ... ) {
 }
 
 /**
- * @brief Writes an information message to the log
+ * @brief   Writes an information message to the log
  *
- * @param message The message to log
- * @return True if the log was successful
+ * @param   message The message to log
+ * @return  True if the log was successful
  */
 bool Log::Info( const std::string& message ) {
 	return Log::get().log( LOG_TYPE_INFO, message );
 }
 
 /**
- * @brief Writes an information message to the log
+ * @brief   Writes an information message to the log
  *
- * @param format The format of the message
- * @param ... Variable arguments
- * @return True if the log was successful
+ * @param   format The format of the message
+ * @param   ... Variable arguments
+ * @return  True if the log was successful
  */
 bool Log::Info( const char* format, ... ) {
 	va_list varArgs;
@@ -269,21 +175,21 @@ bool Log::Info( const char* format, ... ) {
 }
 
 /**
- * @brief Writes a Debug message to the log
+ * @brief   Writes a Debug message to the log
  *
- * @param message The message to log
- * @return True if the log was successful
+ * @param   message The message to log
+ * @return  True if the log was successful
  */
 bool Log::Debug( const std::string& message ) {
 	return Log::get().log( LOG_TYPE_DEBUG, message );
 }
 
 /**
- * @brief Writes an Debug message to the log
+ * @brief   Writes an Debug message to the log
  *
- * @param format The format of the message
- * @param ... Variable arguments
- * @return True if the log was successful
+ * @param   format The format of the message
+ * @param   ... Variable arguments
+ * @return  True if the log was successful
  */
 bool Log::Debug( const char* format, ... ) {
 	va_list varArgs;
@@ -294,19 +200,20 @@ bool Log::Debug( const char* format, ... ) {
 }
 
 /**
- * @brief Peeks at the top element of the function stack
+ * @brief   Peeks at the top element of the function stack
  *
- * @return The top element of the function stack
+ * @return  The top element of the function stack
  */
 std::string Log::Peek() {
 	return Log::get().m_stack.back();
 };
 
 /**
- * @brief Pushes the function stack with the given message
+ * @brief   Pushes the function stack with the given message
  *
- * @param input The message to store in the stack (typically the name of the function)
- * @return True if the stack was successfully pushed
+ * @param   input The message to store in the stack (typically the name of the 
+ *          function)
+ * @return  True if the stack was successfully pushed
  */
 bool Log::Push( const std::string& input ) {
 	if( !input.empty() ) {
@@ -318,9 +225,9 @@ bool Log::Push( const std::string& input ) {
 }
 
 /**
- * @brief Pops the top element off the stack
+ * @brief   Pops the top element off the stack
  *
- * @return The message just popped off the stack
+ * @return  The message just popped off the stack
  */
 std::string Log::Pop() {
 	Log& log = Log::get();
@@ -334,13 +241,14 @@ std::string Log::Pop() {
 }
 
 /**
- * @brief Writes the stack to the log
+ * @brief  Writes the stack to the log
  */
 void Log::PrintStackTrace() {
 	Log& log = Log::get();
 	std::string temp = "---Stack Trace---\n";
 
-	for( std::vector<std::string>::reverse_iterator i = log.m_stack.rbegin(); i != log.m_stack.rend(); ++i) {
+	for( std::vector<std::string>::reverse_iterator i = log.m_stack.rbegin(); 
+			i != log.m_stack.rend(); ++i) {
 		temp += "| " + *i + "\n";
 	}
 
@@ -348,8 +256,99 @@ void Log::PrintStackTrace() {
 	log.write( temp.c_str() );
 }
 /**
- * @brief Copy operator
- * @details Kept private in order to preserve singleton
+ * @brief  Constructor
+ */
+Log::Log()
+	: m_threshold( LOG_TYPE_INFO ),
+	  m_fileName(),
+	  m_stack(),
+	  m_stream() {
+}
+
+/**
+ * @brief  Copy constructor
+ * Kept private in order to preserve singleton
+ */
+Log::Log(const Log&) {
+}
+
+/**
+ * @brief  Destructor
+ * Logs the shut down then closes the file stream
+ */
+Log::~Log() {
+	Finalise();
+}
+
+/**
+ * @brief  Get the singleton instance
+ */
+Log& Log::get() {
+	static Log log;
+	return log;
+}
+
+/**
+ * @brief  Writes the specified message to the console and the log file
+ *
+ * @param  format The format of the message
+ * @param  ... Variable arguments
+ */
+void Log::write( const char* format, ... ) {
+	char buffer[512];
+
+	va_list varArgs;
+	va_start( varArgs, format );
+	vsnprintf( buffer, sizeof(buffer), format, varArgs);
+	va_end( varArgs );
+
+	std::cout << buffer << std::endl;
+	m_stream  << buffer << std::endl;
+}
+
+
+/**
+ * @brief   Logs the specified message with a timestamp and category prefix
+ * The constant TIMESTAMP_BUFFER_SIZE was calculated as the maximum number of 
+ * characters required for the timestamp "[HH:MM:SS MM/DD/YY] ".
+ *
+ * @param   type The category of message to write based on the enum Log::Type
+ * @param   message The message to be sent
+ * @return  True if the log was successful
+ */
+bool Log::log( const Type& type, const std::string& message ) {	
+	if( type <= m_threshold ) {
+		static const int TIMESTAMP_BUFFER_SIZE = 21;
+		char buffer[TIMESTAMP_BUFFER_SIZE];
+		time_t timestamp;
+		time( &timestamp );
+		strftime( buffer, sizeof( buffer ), "%X %x", localtime( &timestamp ) );
+
+		write( "[%s] %s - %s", buffer, TypeToString( type ), message.c_str() );
+		return true;
+	}
+	return false;
+}
+
+/**
+ * @brief   Logs the specified message with a timestamp and category prefix
+ * The constant TIMESTAMP_BUFFER_SIZE was calculated as the maximum number of 
+ * characters required for the timestamp "[HH:MM:SS MM/DD/YY] ".
+ *
+ * @param   type The category of message to write based on the enum Log::Type
+ * @param   format The format of the message
+ * @param   ... Variable arguments
+ * @return  True if the log was successful
+ */
+bool Log::log( const Type& type, const char* format, const va_list& varArgs) {
+	char buffer[512];
+	vsnprintf( buffer, sizeof(buffer), format, varArgs);
+	return log( type, buffer );
+}
+
+/**
+ * @brief  Copy operator
+ * Kept private in order to preserve singleton
  */
 Log& Log::operator=(const Log&) {
 	return *this;
